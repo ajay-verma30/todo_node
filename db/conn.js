ce3f55707f241673d1cdde0080053cfec9187b45
Ajay Verma <35829586+ajay-verma30@users.noreply.github.com>
2024-09-21 11:19:55 +0100

    connectivity changes (Creds updated)

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://aryan2130:kec4lMQJ9BmvOFRj@cluster0.ri4ol.mongodb.net/todo?retryWrites=true&w=majority";
+const uri = "mongodb+srv://aryan2130:ahwm0mNuYS3KDVme@cluster0.ri4ol.mongodb.net/todo?retryWrites=true&w=majority";
 //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 mongoose.connect(uri, {
